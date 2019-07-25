918698dccef2015a2d93451459f336a1d0ce265f
dburnet3 <deniseburnett26@gmail.com>
2019-07-25 18:31:24 -0400

    repositioned my links in the head tag

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 module.exports = [{
     "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo":"https://www.pexels.com/search/woman/",
     "scores":[
         5,
         1,
